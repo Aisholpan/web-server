2f6cf10707e769651c05ba541a8b3d965d5ab829
Aisholpan Kuzdibaeva <aisholpan_2796@mail.ru>
2016-11-21 17:43:46 +0600

    Refactor middleware

--- server.js
+++ server.js
@@ -2,16 +2,9 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req,res,next){
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+
+var middleware = require('./middleware.js');
+
 
 app.use(middleware.logger);
 
@@ -25,4 +18,3 @@ app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function(){
 	console.log('Express server started on port ' + PORT + '!');
 });
-
